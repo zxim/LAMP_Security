db180e816a9d68d9831a15c9af355c519cba6604
xmin <tlaals44@naver.com>
2024-12-26 12:08:10 +0900

    store 수정

--- shop/categories.php
+++ shop/categories.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>스토어</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- 헤더 포함 -->
     <?php include "../login/header.php"; ?>
+    <div class="store-logo">
+        <img src="/project/shop/images/store.png" alt="스토어 로고">
+    </div>
 
-    <!-- 파랑-보라-핑크 텍스트 제목 -->
-    <h1 class="gradient-text">스토어</h1>
-
-    <!-- 카테고리 컨테이너 -->
     <div class="category-container">
         <a href="products.php?category=iPhone">iPhone</a>
         <a href="products.php?category=Mac">Mac</a>
