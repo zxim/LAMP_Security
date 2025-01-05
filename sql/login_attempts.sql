600784728cc22268bf2867ff24de9a5214be2227
xmin <tlaals44@naver.com>
2025-01-06 08:54:42 +0900

    오타

--- sql/login_attempts.sql
+++ sql/login_attempts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE login_attempts (
     ip_address VARCHAR(45) NOT NULL,        -- ipv4, ipv6 주소
-    attempt_count INT NOT NULL,\            -- 실패 횟수
+    attempt_count INT NOT NULL,            -- 실패 횟수
     blocked_until DATETIME DEFAULT NULL,    -- 차단 해제 시간
     last_attempt DATETIME NOT NULL,         -- 마지막 시도 시간
     PRIMARY KEY (ip_address)                -- IP 주소를 기준으로 고유 식별
